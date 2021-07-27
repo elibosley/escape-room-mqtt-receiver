812f7ec17ee5e914d9efad906c977cd2651103b2
Eli Bosley <ekbosley@gmail.com>
2021-07-27 09:54:11 -0700

    Initial Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Connector } from 'mqtt-react-hooks'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Connector brokerUrl=""><Component {...pageProps} /></Connector>
 }
 export default MyApp
